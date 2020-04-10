47060fa6ad9db3b37696d0f867bb9eac3c029bc2
Leandro Cosas <lecosas@gmail.com>
2020-04-10 10:41:55 -0400

    Initial Commit

---
+++ 7LittleWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  7LittleWords
+//
+//  Created by user163948 on 4/10/20.
+//  Copyright © 2020 lecosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
